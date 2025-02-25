2c737e7733a75d038dffe373da1876df279cdebf
Ronak-webdev <shaileshkumarprajapati11200@gmail.com>
2025-02-25 07:16:33 +0000

    dates and times

---
+++ .devcontainer/01_Basics/08_datesinjs.js
@@ -0,0 +1,28 @@
+//Dates
+
+let  myDate = new Date()
+// console.log(myDate.toString());
+
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleTimeString())
+// console.log(typeof myDate)
+
+// let myCreatedDate = new Date(2006, 7, 1, 5,3)
+// let myCreatedDate = new Date("2006-01-08")
+// let myCreatedDate = new Date("01-08-2006")
+// console.log(myCreatedDate.toLocaleString());
+
+// let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000))
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getDay());
